31bd2ca39d27825387a1a9513b90d8ac4a547fb7
Ivan Tomilov <ivan.tomilov@lazada.com>
2015-10-16 17:33:30 +0300

    Changed concurrency count in http-multi.php

--- http-multi.php
+++ http-multi.php
@@ -10,7 +10,7 @@ use GuzzleHttp\Psr7\Request;
  * Configuration
  */
 $totalRequestsToSend = !empty($argv[1]) && is_numeric($argv[1]) ? $argv[1] : 30000;
-$concurrency = 50;
+$concurrency = 24;
 $rpcCallUrl = 'http://95.213.188.198:30390/createProduct';
 ////////////
 
